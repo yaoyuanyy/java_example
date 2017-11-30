cd8c7d47fd6e4629bb48d0e3e83d2db0204caec5
liang.yao <liang.yao@okcoin.com>
2017-11-30 23:34:03 +0800

    convert-between-java-time-localdatetime-and-java-util-date

--- src/main/java/com/yy/util/DateUtil.java
+++ src/main/java/com/yy/util/DateUtil.java
@@ -3,16 +3,18 @@ package com.yy.util;
 import com.yy.enums.DateFormatEnum;
 import org.apache.commons.lang.time.DateFormatUtils;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Objects;
 
 /**
  * Description:
- * <p></p>
- * <pre></pre>
+ * <pre>
+ * 参考：
+ *  1. 国际时间：https://www.mkyong.com/java8/java-8-convert-date-to-localdate-and-localdatetime/
+ *  2. 国内时间：https://stackoverflow.com/questions/19431234/converting-between-java-time-localdatetime-and-java-util-date
+ * </pre>
  * NB.
  * Created by skyler on 2017/11/30 at 下午2:39
  */
@@ -47,9 +49,86 @@ public class DateUtil {
         return result;
     }
 
+    /**
+     * localDateTime Convert to Date with china time
+     * @param localDateTime
+     * @return
+     */
+    public Date localDateTimeConvertDate(LocalDateTime localDateTime){
+        if(Objects.isNull(localDateTime)) {
+            return null;
+        }
+        ZonedDateTime zdt = localDateTime.atZone(ZoneId.systemDefault());
+        Date date = Date.from(zdt.toInstant());
+        return date;
+    }
+
+    /**
+     * Date Convert to localDateTime
+     * @param date
+     * @return
+     */
+    public LocalDateTime ConvertLocalDateTime(Date date){
+        if(Objects.isNull(date)) {
+            return null;
+        }
+        LocalDateTime ldt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        return ldt;
+    }
+
     public static void main(String[] args) {
         String result = formatWithCommon(null);
         System.out.println(result);
+
+        //------ Date -> java.time ------------------------------------------------------
+        //Asia/Kuala_Lumpur +8
+        ZoneId defaultZoneId = ZoneId.systemDefault();
+        System.out.println("System Default TimeZone : " + defaultZoneId);
+
+        //toString() append +8 automatically.
+        Date date = new Date();
+        System.out.println("date : " + date);
+
+        //1. Convert Date -> Instant
+        Instant instant = date.toInstant();
+        System.out.println("instant : " + instant); //Zone : UTC+0
+
+        //2. Instant + system default time zone + toLocalDate() = LocalDate
+        LocalDate localDate = instant.atZone(defaultZoneId).toLocalDate();
+        System.out.println("localDate : " + localDate);
+
+        //3. Instant + system default time zone + toLocalDateTime() = LocalDateTime
+        LocalDateTime localDateTime = instant.atZone(defaultZoneId).toLocalDateTime();
+        System.out.println("localDateTime : " + localDateTime);
+
+        //4. Instant + system default time zone = ZonedDateTime
+        ZonedDateTime zonedDateTime = instant.atZone(defaultZoneId);
+        System.out.println("zonedDateTime : " + zonedDateTime);
+
+
+        //------ java.time -> Date ------------------------------------------------------
+        //Asia/Kuala_Lumpur +8
+        ZoneId defaultZoneId2 = ZoneId.systemDefault();
+        System.out.println("System Default TimeZone : " + defaultZoneId2);
+
+        LocalDate localDate2 = LocalDate.of(2016, 8, 19);
+        Date date2 = Date.from(localDate2.atStartOfDay(defaultZoneId2).toInstant());
+        System.out.println("\n1. LocalDate -> Date");
+        System.out.println("localDate : " + localDate2);
+        System.out.println("date : " + date2);
+
+        LocalDateTime localDateTime2 = LocalDateTime.of(2016,8,19,21,46,31);
+        Date date3 = Date.from(localDateTime2.atZone(defaultZoneId2).toInstant());
+        System.out.println("\n2. LocalDateTime -> Date");
+        System.out.println("localDateTime : " + localDateTime2);
+        System.out.println("date3 : " + date3);
+
+        ZonedDateTime zonedDateTime2 = localDateTime2.atZone(defaultZoneId2);
+        Date date4 = Date.from(zonedDateTime2.toInstant());
+        System.out.println("\n3. ZonedDateTime -> Date");
+        System.out.println("zonedDateTime : " + zonedDateTime2);
+        System.out.println("date4 : " + date4);
+
 
     }
 }
