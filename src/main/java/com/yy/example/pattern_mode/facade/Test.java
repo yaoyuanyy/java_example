3f92d253fa942740cc5a5a03871830718584aa70
liang.yao <liang.yao@okcoin.com>
2017-12-07 22:08:12 +0800

    增加外观模式

---
+++ src/main/java/com/yy/example/pattern_mode/facade/Test.java
@@ -0,0 +1,17 @@
+package com.yy.example.pattern_mode.facade;
+
+/**
+ * Description:
+ * <p></p>
+ * <pre></pre>
+ * NB.
+ * Created by skyler on 2017/12/7 at 下午10:06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        GongJiJin gongJiJin = new GongJiJin();
+        GongJiJinFacade facade = new GongJiJinFacade(gongJiJin);
+        facade.getMoney();
+    }
+}
