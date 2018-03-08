19d954851c531ef3b013b06f0aa93d5794dc4c2b
yaoyuanyy <yaoliang_yl_ok@163.com>
2018-03-08 08:34:16 +0800

    map sort by value

--- src/main/java/com/yy/example/data_structure/MapSortByValue.java
+++ src/main/java/com/yy/example/data_structure/MapSortByValue.java
@@ -1,5 +1,7 @@
 package com.yy.example.data_structure;
 
+import com.google.common.collect.Lists;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -25,7 +27,7 @@ public class MapSortByValue {
 
         //method1(map);
         //method2(map);
-        //method3(map);
+        method3(map);
 
     }
 
@@ -69,21 +71,93 @@ public class MapSortByValue {
     }
 
     /**
-     * 使用Collections.sort(list, valueComparator); + 重写Comparator方法
+     * <pre>
+     *  使用Collections.sort(list, valueComparator); + 重写Comparator方法
+     *  list.sort()
+     *  list.stream().sorted()
+     * </pre>
      *
      * @param map
      */
     public static void method3(final Map<String, Integer> map) {
-        final List list = new ArrayList(map.entrySet());
+        final List<Map.Entry<String, Integer>> list = new ArrayList(map.entrySet());
 
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+        // ------------- 使用Collections.sort()方法排序 ----------------
+        /*Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(final Map.Entry<String, Integer> o1, final Map.Entry<String, Integer> o2) {
 
                 return o2.getValue() - o1.getValue();
             }
         });
+        Collections.sort(list, (Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) -> e1.getValue().compareTo(e2.getValue()));
+        Collections.sort(list, (e1, e2) -> e1.getValue().compareTo(e2.getValue()));
+        Collections.sort(list, Comparator.comparing(Map.Entry::getValue));
+        Collections.sort(list, Map.Entry.comparingByValue());*/
+
+
+        // ------------- list.sort()方法排序 -------------
+        /*list.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(final Map.Entry<String, Integer> o1, final Map.Entry<String, Integer> o2) {
+
+                return o2.getValue() - o1.getValue();
+            }
+        });
+        list.sort((Map.Entry<String, Integer> h1, Map.Entry<String, Integer> h2) -> h1.getValue().compareTo(h2.getValue()));
+        list.sort((h1,h2) -> h1.getValue().compareTo(h2.getValue()));
+        list.sort(Comparator.comparing(Map.Entry::getValue));
+        list.sort(Map.Entry.comparingByValue());*/
 
         list.forEach(System.out::println);
+
+        // ------------- list.stream().sorted()方法排序 -------------
+        List<Map.Entry<String, Integer>> newList = list.stream().sorted(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(final Map.Entry<String, Integer> o1, final Map.Entry<String, Integer> o2) {
+
+                return o2.getValue() - o1.getValue();
+            }
+        }).collect(Collectors.toList());
+//        list.stream().sorted((Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) -> e1.getValue().compareTo(e2.getValue()));
+//        list.stream().sorted((e1, e2) -> e1.getValue().compareTo(e2.getValue()));
+//        list.stream().sorted(Comparator.comparing(Map.Entry::getValue));
+//        list.stream().sorted(Map.Entry.comparingByValue());
+
+        newList.forEach(System.out::println);
+
+        Map<String, Integer> map1 = list.stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(e1-> e1.getKey(), e2 -> e2.getValue(), (e1, e2) -> e1, HashMap::new));
+
+        // ------------- 对自定义对象进行自定义排序方法从而实现排序 -------------
+        List<Human> humans = Lists.newArrayList(new Human("Sarah", 10), new Human("Jack", 12));
+        humans.sort(Human::compareByNameThenAge);
+       // humans.forEach(System.out::println);
     }
+
+
+
+}
+class Human {
+    private String name;
+    private int age;
+
+    public Human() {
+        super();
+    }
+
+    public Human(final String name, final int age) {
+        super();
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public static int compareByNameThenAge(Human lhs, Human rhs) {
+        if (lhs.name.equals(rhs.name)) {
+            return lhs.age - rhs.age;
+        } else {
+            return lhs.name.compareTo(rhs.name);
+        }
+    }
+    // standard getters and setters
 }
