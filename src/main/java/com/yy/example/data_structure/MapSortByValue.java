8acd7546917d855a7b952c6fa274e92102e1a5c8
liang.yao <liang.yao@okcoin.com>
2018-03-08 15:20:08 +0800

    map sort by value

--- src/main/java/com/yy/example/data_structure/MapSortByValue.java
+++ src/main/java/com/yy/example/data_structure/MapSortByValue.java
@@ -73,8 +73,8 @@ public class MapSortByValue {
     /**
      * <pre>
      *  使用Collections.sort(list, valueComparator); + 重写Comparator方法
-     *  list.sort()
-     *  list.stream().sorted()
+     *  list.sort(valueComparator)
+     *  list.stream().sorted(valueComparator)
      * </pre>
      *
      * @param map
@@ -82,7 +82,9 @@ public class MapSortByValue {
     public static void method3(final Map<String, Integer> map) {
         final List<Map.Entry<String, Integer>> list = new ArrayList(map.entrySet());
 
-        // ------------- 使用Collections.sort()方法排序 ----------------
+        /**
+         * ------------- 使用Collections.sort()方法排序 ----------------
+         */
         /*Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(final Map.Entry<String, Integer> o1, final Map.Entry<String, Integer> o2) {
@@ -96,8 +98,10 @@ public class MapSortByValue {
         Collections.sort(list, Map.Entry.comparingByValue());*/
 
 
-        // ------------- list.sort()方法排序 -------------
-        /*list.sort(new Comparator<Map.Entry<String, Integer>>() {
+        /**
+         * ------------- list.sort()方法排序 -------------
+         */
+        list.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(final Map.Entry<String, Integer> o1, final Map.Entry<String, Integer> o2) {
 
@@ -105,38 +109,54 @@ public class MapSortByValue {
             }
         });
         list.sort((Map.Entry<String, Integer> h1, Map.Entry<String, Integer> h2) -> h1.getValue().compareTo(h2.getValue()));
-        list.sort((h1,h2) -> h1.getValue().compareTo(h2.getValue()));
+        list.sort((h1, h2) -> h1.getValue().compareTo(h2.getValue()));
+        // 这种方式调用reversed()，需要getValue是static的，否则报错
         list.sort(Comparator.comparing(Map.Entry::getValue));
-        list.sort(Map.Entry.comparingByValue());*/
+        list.sort(Map.Entry.<String, Integer>comparingByValue().reversed());
 
         list.forEach(System.out::println);
 
-        // ------------- list.stream().sorted()方法排序 -------------
-        List<Map.Entry<String, Integer>> newList = list.stream().sorted(new Comparator<Map.Entry<String, Integer>>() {
+        /**
+         * ------------- list.stream().sorted()方法排序 -------------
+         */
+        final List<Map.Entry<String, Integer>> newList = list.stream().sorted(new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(final Map.Entry<String, Integer> o1, final Map.Entry<String, Integer> o2) {
 
                 return o2.getValue() - o1.getValue();
             }
         }).collect(Collectors.toList());
-//        list.stream().sorted((Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) -> e1.getValue().compareTo(e2.getValue()));
-//        list.stream().sorted((e1, e2) -> e1.getValue().compareTo(e2.getValue()));
-//        list.stream().sorted(Comparator.comparing(Map.Entry::getValue));
-//        list.stream().sorted(Map.Entry.comparingByValue());
+        list.stream().sorted((Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) -> e1.getValue().compareTo(e2.getValue()));
+        list.stream().sorted((e1, e2) -> e1.getValue().compareTo(e2.getValue()));
+        // 这种方式调用reversed()，需要getValue是static的，否则报错
+        list.stream().sorted(Comparator.comparing(Map.Entry::getValue));
+        // (1)
+        list.stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed());
+        // NB. (2)泛型的这种书写方式是错误的，(1)中的书写是对的
+        // list.stream().sorted(Map.Entry<String, Integer>.comparingByValue().reversed());
 
         newList.forEach(System.out::println);
 
-        Map<String, Integer> map1 = list.stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(e1-> e1.getKey(), e2 -> e2.getValue(), (e1, e2) -> e1, HashMap::new));
 
-        // ------------- 对自定义对象进行自定义排序方法从而实现排序 -------------
-        List<Human> humans = Lists.newArrayList(new Human("Sarah", 10), new Human("Jack", 12));
+        /**
+         * ------------- list.stream().sorted()方法排序后直接生成map -------------
+         */
+        final Map<String, Integer> map1 = map.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(e1 -> e1.getKey(),
+                        e2 -> e2.getValue(), (e1, e2) -> e1, HashMap::new));
+
+
+        /**
+         * ------------- 对自定义对象进行自定义排序方法从而实现排序 -------------
+         */
+        final List<Human> humans = Lists.newArrayList(new Human("Sarah", 10), new Human("Jack", 12));
         humans.sort(Human::compareByNameThenAge);
-       // humans.forEach(System.out::println);
+        // humans.forEach(System.out::println);
     }
 
 
-
 }
+
 class Human {
     private String name;
     private int age;
@@ -152,7 +172,7 @@ class Human {
         this.age = age;
     }
 
-    public static int compareByNameThenAge(Human lhs, Human rhs) {
+    public static int compareByNameThenAge(final Human lhs, final Human rhs) {
         if (lhs.name.equals(rhs.name)) {
             return lhs.age - rhs.age;
         } else {
