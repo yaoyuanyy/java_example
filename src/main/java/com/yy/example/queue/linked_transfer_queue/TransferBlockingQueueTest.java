4d19c22f28afafeb83f9d6eb54f0cdf46488cb69
liang.yao <liang.yao@okcoin.com>
2018-04-09 11:38:46 +0800

    transfer queue

--- src/main/java/com/yy/example/queue/linked_transfer_queue/TransferBlockingQueueTest.java
+++ src/main/java/com/yy/example/queue/linked_transfer_queue/TransferBlockingQueueTest.java
@@ -1,10 +1,57 @@
 package com.yy.example.queue.linked_transfer_queue;
 
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.stream.IntStream;
+
 /**
  * Created by yaoliang on 2017/2/27.
  */
 public class TransferBlockingQueueTest {
     public static void main(final String[] args) {
 
+        LinkedTransferQueue<Integer> queue = new LinkedTransferQueue<>();
+        new Thread(new TransferBlockingQueueTest().new Producer(queue)).start();
+        new Thread(new TransferBlockingQueueTest().new Customer(queue)).start();
+
+    }
+
+    class Producer implements Runnable{
+
+        private LinkedTransferQueue<Integer> queue;
+
+        public Producer(LinkedTransferQueue<Integer> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            IntStream.range(0, 5).forEach(a -> {
+                System.out.println("producer 等待生产...:"+a);
+                queue.add(a);
+                System.out.println("producer 生产完成:"+a);
+            });
+        }
+    }
+
+    class Customer implements Runnable{
+
+        private LinkedTransferQueue<Integer> queue;
+
+        public Customer(LinkedTransferQueue<Integer> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            IntStream.range(0, 5).forEach(a -> {
+                try {
+                    System.out.println("Customer 等待消费...:"+a);
+                    queue.take();
+                    System.out.println("Customer 消费完成:"+a);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 }
