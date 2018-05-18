d18fd70bb3a2a37cde285df2598d15fe743a477f
liang.yao <liang.yao@okcoin.com>
2018-05-18 18:04:28 +0800

    GC test

---
+++ src/main/java/com/yy/example/java_base/gc/GCTest.java
@@ -0,0 +1,35 @@
+package com.yy.example.java_base.gc;
+
+/**
+ * Description:
+ * <p></p>
+ * <pre>
+ *     运行时配置: -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:SurvivorRatio=8 -XX:+PrintGCDetails
+ *     refer to: 深入理解java虚拟机
+ *                               第三章 3.5节-内存分配与回收策略
+ *                                      代码清单3-3
+ * </pre>
+ * NB.
+ * Created by skyler on 2018/5/18 at 下午3:02
+ */
+public class GCTest {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void testAllocation() {
+        final byte[] allocation1;
+        final byte[] allocation2;
+        final byte[] allocation3;
+        final byte[] allocation4;
+
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation4 = new byte[4 * _1MB];
+
+    }
+
+    public static void main(final String[] args) {
+        testAllocation();
+    }
+}
