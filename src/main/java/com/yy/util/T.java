591ce0f58e7610a61d524ec7ddfa92b71216c40d
yaoyuanyy <yaoliang_yl_ok@163.com>
2018-07-09 17:45:17 +0800

    add crypt  util

---
+++ src/main/java/com/yy/util/T.java
@@ -0,0 +1,32 @@
+package com.yy.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Description:
+ * <p></p>
+ * <pre></pre>
+ * NB.
+ * Created by skyler on 2018/7/9 at 下午4:48
+ */
+public class T {
+
+    public static void main(String[] args) {
+        String filetext = "guangsha;1.1.0;iOS;11.3;iPhone 8";
+        //正则表达式，取=和|之间的字符串，不包括=和|
+        Pattern p = Pattern.compile("\\;(.*?)\\;");
+        Matcher m = p.matcher(null);
+
+        String des = null;
+        if(m.find()) {
+            des = m.group(1);
+            System.out.println(des);
+
+        }
+
+        System.out.println(("1.1.0".compareTo(des)));
+
+
+    }
+}
