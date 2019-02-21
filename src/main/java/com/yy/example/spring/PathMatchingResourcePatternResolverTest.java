f00340633756edd5818674ddb4f89bdba0e887e4
yaoyuanyy <yaoliang_yl_ok@163.com>
2019-02-21 12:33:35 +0800

    完善PathMatchingResourcePatternResolverTest例子，更清晰的说明

--- src/main/java/com/yy/example/spring/PathMatchingResourcePatternResolverTest.java
+++ src/main/java/com/yy/example/spring/PathMatchingResourcePatternResolverTest.java
@@ -4,14 +4,15 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.util.ResourceUtils;
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
- * Description:
+ * Description: 读取classpath下或指定目录下的test.properties内容
  * <p></p>
  * <pre></pre>
  * NB.
@@ -21,22 +22,41 @@ public class PathMatchingResourcePatternResolverTest {
 
     public static void main(String[] args) throws IOException {
 
-//        PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
-//        String url = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + "com/yy/" + "test.properties";
-//        Resource[] resources = resourcePatternResolver.getResources(url);
-//        String path = "";
-//        for (Resource resource : resources) {
-//            path = resource.getURL().getPath();
-//            System.out.println("path:" + path);
-//        }
-//        Resource resource = resourcePatternResolver.getResource("file:" + path);
-//        System.out.println(IOUtils.toString(resource.getInputStream(),"utf-8"));
+       PathMatchingResourcePatternResolverTest test = new PathMatchingResourcePatternResolverTest();
+       test.readFromMoreUrl();
 
+    }
 
+    public Map<String, Object> readFromMoreUrl() throws IOException {
+        Map map = new HashMap();
+        PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        String url = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + "com/yy/" + "test.properties";
+        Resource[] resources = resourcePatternResolver.getResources(url);
+        String path = "";
+        for (Resource resource : resources) {
+            System.out.println("path:" + resource.getURL().getPath());
+            /**
+             * 这里是直接从inputStream打印出来了，实际用的时候可以使用
+             * Properties properties = new Properties();
+             * Enumeration enumeration = properties.load(resource.getInputStream());
+             * 进一步使用
+             */
+            System.out.println(IOUtils.toString(resource.getInputStream(), "utf-8"));
+        }
+
+        return map;
+
+    }
+
+    public Map<String, Object> readFromSingleUrl() throws IOException {
+
+        Map map = new HashMap();
         // resources下建立一个com.yy包，包下建个文件：test.properties
         PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
         String url = ResourcePatternResolver.CLASSPATH_URL_PREFIX + "com/yy/" + "test.properties";
         Resource resource = resourcePatternResolver.getResource(url);
+
+        // 可以直接打印InputStream的内容
         // String text = IOUtils.toString(resource.getInputStream(),"utf-8");
 
         Properties properties = new Properties();
@@ -44,8 +64,11 @@ public class PathMatchingResourcePatternResolverTest {
 
         Enumeration enumeration = properties.propertyNames();
         while (enumeration.hasMoreElements()) {
-            String key = (String)enumeration.nextElement();
-            System.out.println("key:"+key +" value:"+properties.getProperty(key));
+            String key = (String) enumeration.nextElement();
+            map.put(key, properties.getProperty(key));
+            System.out.println("key:" + key + " value:" + properties.getProperty(key));
         }
+
+        return map;
     }
 }
