f4ce9cce8afe4c42089477bcfb8bf23b4e172702
yaoyuanyy <yaoliang_yl_ok@163.com>
2019-06-03 10:42:09 +0800

    增加动态代理类存储到硬盘class文件

--- src/main/java/com/yy/rest/UserController.java
+++ src/main/java/com/yy/rest/UserController.java
@@ -24,39 +24,9 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
-//    ThreadFactory factory = new BasicThreadFactory.Builder().namingPattern("test-multi-thread-%d").build();
-//
-//    ThreadPoolExecutor executor = new ThreadPoolExecutor(210,220,10, TimeUnit.MILLISECONDS,
-//            new SynchronousQueue<>());
-
-    private static int i = 0;
-
-    @RequestMapping("/hello")
-    public String hello(final Person person) {
-        try{
-            int i = 10/0;
-        }catch (Exception e){
-            log.error("方法{} error", "hello", e);
-        }
-//    for (int i = 0; i < 1000; i++){
-//        executor.execute(() -> {
-//            try {
-//                Thread.sleep(100);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println("current-thread:"+Thread.currentThread().getName());
-//        });
-//    }
-        System.out.println(person);
-
-        return "success";
-    }
-
     @RequestMapping("/test")
     public String test() {
-        i++;
-        System.out.println("^^^:"+i);
+        System.out.println("^^^:");
 
         return "success";
     }
