6a9f165caba6ae5dd3a3f4869885b597727feb19
yaoliang004 <yaoliang004@ke.com>
2021-07-18 21:13:08 +0800

    java对象内存布局

--- src/main/java/com/yy/example/jol/TestMain.java
+++ src/main/java/com/yy/example/jol/TestMain.java
@@ -3,10 +3,12 @@ package com.yy.example.jol;
 import org.openjdk.jol.info.ClassLayout;
 import org.openjdk.jol.vm.VM;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Description:
  * <pre>
- *
+ *  https://www.bilibili.com/video/BV1xK4y1C7aT?p=3&spm_id_from=pageDriver
  * </pre>
  * NB.
  *
@@ -21,6 +23,15 @@ public class TestMain {
         System.out.println(ClassLayout.parseClass(Consumer.class).toPrintable());
         System.out.println("===============");
         System.out.println(ClassLayout.parseInstance(new Consumer()).toPrintable());
+
+        System.out.println("#################");
+        Object o = new Object();
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+
+        synchronized (o) {
+            // 关注下打印出来的字节，看看加锁前后的区别
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }
 
     }
 }
